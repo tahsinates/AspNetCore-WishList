0f253071b2ac4926e7547c1cb9a6287f48972fe2
Tahsin Ates <tahsinates@gmail.com>
2020-04-06 19:29:51 +0100

    completed

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -19,7 +19,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<ApplicationDbContext>(options =>  options.UseInMemoryDatabase("WishList"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
 
         }
 
